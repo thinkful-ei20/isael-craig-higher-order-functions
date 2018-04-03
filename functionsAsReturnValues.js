e873888effd69864dd85b0299ba50650d5bd3ec7
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-04-03 13:37:26 -0600

    made functionsAsReturnValues.js

---
+++ functionsAsReturnValues.js
@@ -0,0 +1,21 @@
+'use strict';
+function warningCounter(typeOfWarning){
+    let warningCounter = 0;
+    return (location) => {
+        warningCounter++;
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}`);
+        console.log(`The ${typeOfWarning} hazard has triggered ${warningCounter} time(s) today!`);
+    };
+}
+const rockWarnings = warningCounter("Rock Warnings");
+const wetRoad = warningCounter("Wet Road");
+const thinIce = warningCounter("Thin Ice")
+rockWarnings("Main St and Pacific Ave");
+wetRoad('Centinela Ave and Olympic Blvd');
+thinIce("Ice on Federal Blvd");
+
+thinIce();
+wetRoad();
+rockWarnings();
+rockWarnings();
+thinIce();
