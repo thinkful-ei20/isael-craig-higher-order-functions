25fab6345101a55d08b50efa7f62466090f282a0
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-04-03 18:07:04 -0600

    changed to a proper reduce

--- reduce.js
+++ reduce.js
@@ -1,6 +1,22 @@
 'use strict';
 let string = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
 
+   let newStr = string.split(" ").reduce((a, b) => {
+
+        if(b.length === 3) return a + " ";
+        else return a + b[b.length-1].toUpperCase();
+
+    },"");
+ 
+    
+
+console.log(newStr);
+
+/* original 
+
+'use strict';
+let string = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
+
 function reducer(str){
     let newString = '';
     let result = str.split(" ");
@@ -19,3 +35,6 @@ function reducer(str){
     
 }
 console.log(reducer(string));
+
+
+*/
